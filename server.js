15f836e48102930494a4dd5aa2df4e93d31f4b90
pelex04 <kingsleychideru1404@gmail.com>
2025-11-27 11:23:23 +0200

    Added cors

--- server.js
+++ server.js
@@ -8,12 +8,8 @@ require('dotenv').config();
 
 const app = express();
 const port = 3000;
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors()); 
+
 
 app.use(express.json());
 
